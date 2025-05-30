551e3a5c4e84469e074726a56163feb7970155a3
TWhilst <tochethestrict@gmail.com>
2025-05-30 20:42:25 +0100

    made changes

--- terraform/routes.tf
+++ terraform/routes.tf
@@ -7,21 +7,21 @@ resource "aws_route_table" "Project1_rt" {
   }
 }
 
-resource "aws_route_table" "Project1_rt_private" {
-  vpc_id = aws_vpc.Project1.id
+# resource "aws_route_table" "Project1_rt_private" {
+#   vpc_id = aws_vpc.Project1.id
 
-  tags = {
-    Name = "main-rt-private"
-  }
-}
+#   tags = {
+#     Name = "main-rt-private"
+#   }
+# }
 
-resource "aws_route_table" "Project1_rt_private_b" {
-  vpc_id = aws_vpc.Project1.id
+# resource "aws_route_table" "Project1_rt_private_b" {
+#   vpc_id = aws_vpc.Project1.id
 
-  tags = {
-    Name = "main-rt-private_b"
-  }
-}
+#   tags = {
+#     Name = "main-rt-private_b"
+#   }
+# }
 
 /// Route
 resource "aws_route" "Project1_route" {
@@ -30,17 +30,17 @@ resource "aws_route" "Project1_route" {
   gateway_id             = aws_internet_gateway.Project1_igw.id
 }
 
-resource "aws_route" "Project1_route_private" {
-  route_table_id         = aws_route_table.Project1_rt_private.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_nat_gateway.Project1_nat.id
-}
+# resource "aws_route" "Project1_route_private" {
+#   route_table_id         = aws_route_table.Project1_rt_private.id
+#   destination_cidr_block = "0.0.0.0/0"
+#   gateway_id             = aws_nat_gateway.Project1_nat.id
+# }
 
-resource "aws_route" "Project1_route_private_b" {
-  route_table_id         = aws_route_table.Project1_rt_private_b.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_nat_gateway.Project1_nat_b.id
-}
+# resource "aws_route" "Project1_route_private_b" {
+#   route_table_id         = aws_route_table.Project1_rt_private_b.id
+#   destination_cidr_block = "0.0.0.0/0"
+#   gateway_id             = aws_nat_gateway.Project1_nat_b.id
+# }
 
 // Route Table Association
 resource "aws_route_table_association" "Project1_rta" {
@@ -50,6 +50,16 @@ resource "aws_route_table_association" "Project1_rta" {
 
 resource "aws_route_table_association" "Project1_rta_b" {
   subnet_id      = aws_subnet.Project1_public_b.id
+  route_table_id = aws_route_table.Project1_rt.id
+}
+
+resource "aws_route_table_association" "Project1_rta1" {
+  subnet_id      = aws_subnet.Project1_public1.id
+  route_table_id = aws_route_table.Project1_rt.id
+}
+
+resource "aws_route_table_association" "Project1_rta1_b" {
+  subnet_id      = aws_subnet.Project1_public1_b.id
   route_table_id = aws_route_table.Project1_rt.id
 }
 
